ce4f7736c587e58176c77c7d446f200e55057537
Zhusee Zhang <zhusee2@gmail.com>
2012-06-26 23:12:41 +0800

    Implements the injected script part for context menu

--- Shortly.safariextension/contextMenu.js
+++ Shortly.safariextension/contextMenu.js
@@ -1,6 +1,27 @@
 function contextMenuHandler(event) {
-  safari.self.tab.setContextMenuEventUserInfo(event, event.target.nodeName);
-  console.log(event);
+  var nodeName = event.target.nodeName,
+      menuToResponse = new Array();
+
+  if (event.target.parentElement.nodeName === 'A') {
+    menuToResponse.push({type: 'link', src: event.target.parentElement.href});
+  }
+  switch (nodeName) {
+    case 'A':
+      menuToResponse.push({type: 'link', src: event.target.href});
+      break;
+    case 'IMG':
+      menuToResponse.push({type: 'image', src: event.target.src});
+      break;
+    default:
+      break;
+  }
+  
+  if (menuToResponse.length == 0) {
+    event.preventDefault(); //Hide the context menus.
+  }
+  
+  console.log(menuToResponse);
+  safari.self.tab.setContextMenuEventUserInfo(event, menuToResponse);
 }
 
 document.addEventListener('contextmenu', contextMenuHandler);
