996129a74e17f6f4a0039b03463376b81d1ca8ef
Teng-hao Chang <zhusee2@gmail.com>
2015-07-04 00:36:08 +0800

    Basic custom endpoint support

--- source/shorten-services/custom-shortener.js
+++ source/shorten-services/custom-shortener.js
@@ -1,5 +1,23 @@
 export default class CustomEndpointShortener {
   getShortlink(longUrl) {
-    return Promise.resolve('custom endpoint');
+    var queryAPI = safari.extension.settings.customEndpoint || '';
+
+    if (queryAPI === '') {
+      return Promise.reject('Error: bad custom endpoint.')
+    } else {
+      queryAPI = queryAPI.replace(/=%@/, '=' + encodeURIComponent(longUrl));
+
+      return fetch(queryAPI)
+        .then( response => response.json() )
+        .then( resultJSON => {
+          var shortlinkFound = resultJSON.shortlink || resultJSON.shorturl || resultJSON.link || resultJSON.url || resultJSON.result || '';
+
+          if (typeof shortlinkFound === 'string' && shortlinkFound != '') {
+            return shortlinkFound;
+          } else {
+            return Promise.reject('Error: unable to parse result from custom endpoint.')
+          }
+        })
+    }
   }
 }
