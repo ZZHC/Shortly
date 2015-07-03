5c15030bbb23d69dbba8648d7a3f9b14e3949be3
Teng-hao Chang <zhusee2@gmail.com>
2015-07-04 00:29:38 +0800

    Basic goo.gl shortener

--- source/shorten-services/google-shortener.js
+++ source/shorten-services/google-shortener.js
@@ -1,5 +1,33 @@
+import apiKeys from '../api-keys'
+
+const API_ENDPOINT = 'https://www.googleapis.com/urlshortener/v1/url'
+const API_KEY = apiKeys.google
+
 export default class GoogleShortener {
   getShortlink(longUrl) {
-    return Promise.resolve('goo.gl');
+    return this._shortenWithApiKey(longUrl);
+  }
+
+  _shortenWithApiKey(longUrl) {
+    var queryAPI = API_ENDPOINT + `?key=${ API_KEY }`;
+
+    return fetch(queryAPI, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({longUrl})
+    })
+      .then( response => response.json() )
+      .then( resultJSON => {
+        if (resultJSON.id) {
+          return resultJSON.id
+        } else if (resultJSON.error) {
+          return Promise.reject(`Error ${resultJSON.error.code}: ${resultJSON.error.message}`)
+        } else {
+          return Promise.reject('Unknown error: goo.gl')
+        }
+      })
   }
 }
