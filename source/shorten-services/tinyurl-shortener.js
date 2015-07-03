efaedd94f4ca458852d7a29e61daf7799f9760c5
Teng-hao Chang <zhusee2@gmail.com>
2015-07-03 18:02:11 +0800

    TinyURL shortener

--- source/shorten-services/tinyurl-shortener.js
+++ source/shorten-services/tinyurl-shortener.js
@@ -1,5 +1,17 @@
+const URL_PATTERN = /^http:\/\/\w*\.?tinyurl\.com\//
+
 export default class TinyUrlShortener {
   getShortlink(longUrl) {
-    return Promise.resolve('tinyurl.com');
+    var queryAPI = 'http://tinyurl.com/api-create.php?url=' + encodeURIComponent(longUrl);
+
+    return fetch(queryAPI)
+      .then( response => response.text() )
+      .then( result => {
+        if(result.match(URL_PATTERN)) {
+          return result
+        } else {
+          return Promise.reject(`Error: TinyURL responding ${result}`)
+        }
+      })
   }
 }
