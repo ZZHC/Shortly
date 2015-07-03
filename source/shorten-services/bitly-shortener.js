7b42fbf0b7a17dd722e437836985718d28d486d0
Teng-hao Chang <zhusee2@gmail.com>
2015-07-03 18:16:15 +0800

    Bitly shortener

--- source/shorten-services/bitly-shortener.js
+++ source/shorten-services/bitly-shortener.js
@@ -1,5 +1,28 @@
+import apiKeys from '../api-keys'
+
+const DEFAULT_LOGIN = 'zzhc';
+const DEFAULT_API_KEY = apiKeys.bitly;
+
 export default class BitlyShortener {
   getShortlink(longUrl) {
-    return Promise.resolve('bit.ly');
+    var queryAPI,
+        userLogin = safari.extension.secureSettings.bitlyUsername || '',
+        userApiKey = safari.extension.secureSettings.bitlyAPIKey || '';
+
+    if ((userLogin === '') && (userApiKey === '')) {
+      userLogin = DEFAULT_LOGIN;
+      userApiKey = DEFAULT_API_KEY;
+    }
+    queryAPI = `http://api.bitly.com/v3/shorten?login=${ userLogin }&apiKey=${ userApiKey }&longUrl=${ encodeURIComponent(longUrl) }`
+
+    return fetch(queryAPI)
+      .then( response => response.json() )
+      .then( resultJSON => {
+        if (resultJSON.status_code === 200) {
+          return resultJSON.data.url;
+        } else {
+          return Promise.reject(`Error ${resultJSON.status_code}: ${resultJSON.status_txt}`)
+        }
+      })
   }
 }
