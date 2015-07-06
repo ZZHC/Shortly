37b830d30a30a95897d3ce60ddce26040f66207f
Teng-hao Chang <zhusee2@gmail.com>
2015-07-06 09:20:39 +0800

    Setup temporary popover and display

--- source/displays/popover-display.js
+++ source/displays/popover-display.js
@@ -1,10 +1,48 @@
 class PopoverDisplay {
+  constructor() {
+    this._toolbarItem = this.getToolbarItemOnActiveWindow();
+    this._popover = this.getPopover();
+  }
+
   displayShortlink(shortlink) {
-    console.log(shortlink);
+    this.setupTemporaryPopover();
+    this._toolbarItem.showPopover();
   }
 
   displayError(errorMsg) {
     console.log(errorMsg);
+  }
+
+  getPopover() {
+    for (let i in safari.extension.popovers) {
+      let popover = safari.extension.popovers[i];
+      if (popover.identifier === 'popoverResult') return popover;
+    }
+  }
+
+  getToolbarItemOnActiveWindow() {
+    for (let i in safari.extension.toolbarItems) {
+      let toolbarItem = safari.extension.toolbarItems[i];
+      if (toolbarItem.browserWindow === safari.application.activeBrowserWindow) return toolbarItem;
+    }
+  }
+
+  setupTemporaryPopover() {
+    var toolbarMenu = this._toolbarItem.menu;
+
+    var popoverSelfBomb = (event) => {
+      /* Remove popover and hook back the menu
+       * after popover being closed */
+      if (event.srcElement === this._popover.contentWindow) {
+        this._toolbarItem.popover = null;
+        this._toolbarItem.menu = toolbarMenu;
+        this._popover.contentWindow.removeEventListener('blur', popoverSelfBomb, false);
+      }
+    }
+
+    this._toolbarItem.menu = null;
+    this._toolbarItem.popover = this._popover;
+    this._popover.contentWindow.addEventListener('blur', popoverSelfBomb, false);
   }
 }
 
