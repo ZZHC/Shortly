69ff77b382375424aee83f22eba687a7c8f5ee97
Teng-hao Chang <zhusee2@gmail.com>
2015-07-11 00:39:30 +0800

    Make TaskQueue an EventEmitter

--- source/components/task-queue.js
+++ source/components/task-queue.js
@@ -1,10 +1,14 @@
-class TaskQueue {
+import {EventEmitter} from 'events'
+
+class TaskQueue extends EventEmitter {
   constructor(initArray=[]) {
+    super();
     this._queueArray = initArray.slice();
   }
 
   push(task) {
     this._queueArray.push(task);
+    this.emit('change');
   }
 
   remove(task) {
@@ -12,6 +16,16 @@ class TaskQueue {
     if (queueIndex < 0) return false;
 
     this._queueArray.slice(queueIndex, 1);
+    this.emit('change');
+  }
+
+  findWithBrowserWindow(browserWindow) {
+    var i, task;
+
+    for (i = 0; i < this._queueArray.length; i++) {
+      task = this._queueArray[i];
+      if (task.browserWindow === browserWindow) return task;
+    }
   }
 
   toArray() {
