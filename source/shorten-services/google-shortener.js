dfb1ed768bb3545dc5309fc6081df881bf9a25ab
Teng-hao Chang <zhusee2@gmail.com>
2015-07-14 00:19:21 +0800

    Shorten goo.gl with OAuth token if specified

--- source/shorten-services/google-shortener.js
+++ source/shorten-services/google-shortener.js
@@ -1,11 +1,16 @@
 import apiKeys from '../api-keys'
+import GoogleOAuth from '../components/google-oauth'
 
 const API_ENDPOINT = 'https://www.googleapis.com/urlshortener/v1/url'
 const API_KEY = apiKeys.google
 
 export default class GoogleShortener {
   getShortlink(longUrl) {
-    return this._shortenWithApiKey(longUrl);
+    if (safari.extension.settings.useGoogleOAuth) {
+      return this._shortenWithOAuthToken(longUrl);
+    } else {
+      return this._shortenWithApiKey(longUrl);
+    }
   }
 
   _shortenWithApiKey(longUrl) {
@@ -18,13 +23,30 @@ export default class GoogleShortener {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({longUrl})
+    }).then(this._processResponse);
+  }
+
+  _shortenWithOAuthToken(longUrl) {
+    return GoogleOAuth.getAuthenticateHeader().then( authString => {
+      return fetch(API_ENDPOINT, {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': authString
+        },
+        body: JSON.stringify({longUrl})
+      }).then(this._processResponse);
     })
-      .then( response => response.json() )
-      .then( resultJSON => {
-        if (resultJSON.id) {
-          return resultJSON.id
-        } else if (resultJSON.error) {
-          return Promise.reject(`Error ${resultJSON.error.code}: ${resultJSON.error.message}`)
+  }
+
+  _processResponse(response) {
+    return response.json()
+      .then( resultJson => {
+        if (resultJson.id) {
+          return resultJson.id
+        } else if (resultJson.error) {
+          return Promise.reject(`Error ${resultJson.error.code}: ${resultJson.error.message}`)
         } else {
           return Promise.reject('Unknown error: goo.gl')
         }
