2cf96d10da6ba252d151f73540b6e195fe346012
Teng-hao Chang <zhusee2@gmail.com>
2015-07-15 09:16:38 +0800

    Fix toolbar item animate frames

--- source/ui/toolbar-item-animator.js
+++ source/ui/toolbar-item-animator.js
@@ -52,8 +52,9 @@ class ToolbarItemAnimator {
   }
 
   _getNextFrame(frame) {
-    var nextFrame = (frame % 12) + 2; // [1...12] + 1
+    var nextFrame = (frame % 12) + 1; // [1...12]
     this._toolbarItem.image = `${BASE_URI}spin/${frame}.png`;
+    console.log(this._toolbarItem.image);
 
     if (this._status === PLAYING) {
       setTimeout( () => { this._getNextFrame(nextFrame) }, SPIN_SPEED);
