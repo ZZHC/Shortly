0a7d6c43527e139d8eb06fd4b02e4bcab66f27a4
Teng-hao Chang <zhusee2@gmail.com>
2015-07-18 09:00:59 +0800

    I18n utility

---
+++ source/locale.js
@@ -0,0 +1,30 @@
+const LocaleLib = {
+  toolbarBtn: {
+    label: {
+      'default': 'Shorten',
+      'zh-tw': '縮短網址',
+      'zh-cn': '缩短地址',
+      'ja-jp': 'アドレスを短縮',
+      'it-it': 'Accorcia',
+      'fr-fr': 'Raccourcir'
+    },
+    paletteLabel: {
+      'default': 'Shorten',
+      'zh-tw': '縮短網址',
+      'zh-cn': '缩短地址',
+      'ja-jp': 'アドレスを短縮',
+      'it-it': 'Accorcia',
+      'fr-fr': 'Raccourcir'
+    },
+    tooltip: {
+      'default': 'Get the shortlink for current page',
+      'zh-tw': '縮短目前網頁的網址',
+      'zh-cn': '缩短当前页面的地址',
+      'ja-jp': '現在のページのアドレスを短縮します',
+      'it-it': 'Ottieni un indirizzo breve della pagina corrente',
+      'fr-fr': "Obtenir l'adresse raccourcie de la page active",
+    }
+  }
+}
+
+export default LocaleLib
