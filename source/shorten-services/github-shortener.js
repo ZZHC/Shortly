343cbe85b7014931d84a2f1cc13211ffd3fd9cfc
Teng-hao Chang <zhusee2@gmail.com>
2015-07-22 00:51:25 +0800

    Fix name of GithubShortener

--- source/shorten-services/github-shortener.js
+++ source/shorten-services/github-shortener.js
@@ -1,6 +1,6 @@
 const URL_PATTERN = /http(s)?:\/\/(gist\.)?github\.com/;
 
-export default class FlickrShortener {
+export default class GithubShortener {
   getShortlink(longUrl) {
     return fetch('http://git.io', {
       method: 'post',
