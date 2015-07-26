9bc64b10780e5cca883511592ed5c19264a1ff9c
Teng-hao Chang <zhusee2@gmail.com>
2015-07-26 09:51:11 +0800

    Add locale for welcome page

--- source/locale/welcome-page.js
+++ source/locale/welcome-page.js
@@ -1,3 +1,50 @@
 export default {
-  welcome: {}
+  welcome: {
+    langProp: {
+      'default': 'en',
+      'zh-tw': 'zh-tw',
+      'zh-cn': 'zh-cn'
+    },
+    title: {
+      'default': 'Welcome to Shortly.',
+      'zh-tw': '歡迎使用 Shortly',
+      'zh-cn': '欢迎使用 Shortly'
+    },
+    lead: {
+      'default': 'You can now connect to your accounts below to save all goo.gl or Bitly-powered shortlinks into your own accounts, so you can track all your shortlinks whenever you want.',
+      'zh-tw': '您現在可以在下方連接您的帳戶，就可以把所有 goo.gl 或是透過 Bitly 縮短的短網址都存在您自己的帳戶裡。這樣您日後隨時都能追蹤您所有的短網址記錄。'
+    },
+    assurance: {
+      'default': 'Shortly will never save your password.',
+      'zh-tw': 'Shortly 絕對不會儲存您的密碼。'
+    },
+    connect: {
+      'default': 'Connect',
+      'zh-tw': '連接帳戶'
+    },
+    continue: {
+      'default': 'Continue',
+      'zh-tw': '繼續'
+    },
+    google: {
+      title: {
+        'default': 'Connect to my Google account',
+        'zh-tw': '連接我的 Google 帳戶'
+      },
+      helpText: {
+        'default': 'You can then find all your <i>goo.gl</i> links over <a href="https://goo.gl/" target="_blank">Google URL Shortener</a>.',
+        'zh-tw': '之後您可以在 <a href="https://goo.gl/" target="_blank">Google URL Shortener</a> 找到您所有的 <i>goo.gl</i> 短網址。'
+      }
+    },
+    bitly: {
+      title: {
+        'default': 'Connect to my Bitly account',
+        'zh-tw': '連接我的 Bitly 帳戶'
+      },
+      helpText: {
+        'default': 'You can then find all your <i>bit.ly</i> or links like <i>on.fb.me</i> over <a href="https://bitly.com/" target="_blank">Bitly.com</a>.',
+        'zh-tw': '之後您可以在 <a href="https://bitly.com/" target="_blank">Bitly.com</a> 找到您所有的 <i>bit.ly</i> 或是如 <i>on.fb.me</i> 之類的短網址。'
+      }
+    }
+  }
 }
