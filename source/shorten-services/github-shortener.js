4a571e72365963c3249b85805685a94c07abaae8
Teng-hao Chang <zhusee2@gmail.com>
2016-04-07 09:53:45 +0800

    Fix github shortener sometimes unable to create

--- source/shorten-services/github-shortener.js
+++ source/shorten-services/github-shortener.js
@@ -2,9 +2,9 @@ const URL_PATTERN = /http(s)?:\/\/(gist\.)?github\.com/;
 
 export default class GithubShortener {
   getShortlink(longUrl) {
-    return fetch('http://git.io', {
+    return fetch('https://git.io', {
       method: 'post',
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
       body: 'url=' + encodeURIComponent(longUrl)
     })
       .then( response => {
